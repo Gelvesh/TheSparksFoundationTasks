90f593d7f8bbbee76cb073c96f92eb3e740e212c
Gelvesh <57354726+Gelvesh@users.noreply.github.com>
2021-04-07 11:25:10 +0530

    Add files via upload
    Task 1 of Data Science and Business Analytics (Prediction using Supervised ML)

---
+++ TSF_Task1.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np  
+import matplotlib.pyplot as plt  
+
+dataset = pd.read_csv(r'D:\Android\Practice\student_scores.csv')
+#print(dataset)
+#print(dataset.head())
+#print(dataset.shape)
+
+dataset.plot(x='Hours', y='Scores', style='o')
+plt.title('Hours vs Percentage')
+plt.xlabel('Hours Studied')
+plt.ylabel('Percentage Score')
+plt.show()
+
+X = dataset.iloc[:, :-1].values    
+y = dataset.iloc[:, 1].values  
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+from sklearn.linear_model import LinearRegression    
+regressor = LinearRegression()    
+regressor.fit(X_train, y_train)   
+  
+print("Training.......")
+
+line = regressor.coef_*X+regressor.intercept_  
+plt.scatter(X, y)  
+plt.plot(X, line)
+plt.show() 
+
+print(X_test)   
+y_pred = regressor.predict(X_test)  
+
+df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})    
+print(df)
+
+hours = [[7]]  
+own_pred = regressor.predict(hours)  
+print("Number of hours = {}".format(hours))  
+print("Prediction Score = {}".format(own_pred[0]))  
